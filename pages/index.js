1ceb2600b137f188ec5d9a1ae27134cffbfef45d
123jossarts <abelnsingi9@gmail.com>
2025-04-05 18:48:15 +0100

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Bienvenue sur Skhill Institut.an</h1>
+      <p>Votre plateforme de formation en ligne certifiante</p>
+    </div>
+  );
+}
